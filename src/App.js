91a375898106ced6ba2b181062341c9798e7a3a9
gold <narnavg@gmail.com>
2022-07-18 13:12:15 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -5,17 +5,29 @@ import { useSelector, useDispatch } from "react-redux";
 import {
   decrement,
   increment,
-  selectCount,
+  selectCount,incrementTwo
 } from "./counterSlice";
+
+import {
+     login, logout,selectLog
+
+  } from "./loggedSlice";
+
 function App() {
   const count = useSelector(selectCount);
+  const log = useSelector(selectLog);
+
   const dispatch = useDispatch();
   return (
     <div className="App">
       <header className="App-header">
         <button onClick={() => dispatch(increment())}>+</button>
         <button onClick={() => dispatch(decrement())}>-</button>
+        <button onClick={() => dispatch(incrementTwo())}>++</button>
+        <button onClick={() => dispatch(login())}>Login</button>
+        <button onClick={() => dispatch(logout())}>Logout</button>
         {count}
+        <hr></hr>{log ? "log":"not log"}
         <DisplayUpd></DisplayUpd>
       </header>
     </div>
